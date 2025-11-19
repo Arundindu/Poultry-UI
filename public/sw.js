a8189a467ebc3ba4b0d0046e338df4e33fc53cae
Dindu ArunKumar Reddy <arunkumar.dindu@knowledgelens.com>
2025-11-19 16:45:35 +0530

    Test

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,11 @@
+import Toaster from "../src/Shared/Utils/Toaster";
+
 self.addEventListener("push", function (event) {
     console.log('Pushing',event)
+    Toaster.success(event,'success')
+    Toaster.success(event.title,'success')
+    Toaster.success(event.body,'success')
+    
     const data = event;
     // const data = {};
 
