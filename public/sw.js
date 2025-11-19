d1c26f300d32cdff90c446f1c3d3ade675a6138c
Dindu ArunKumar Reddy <arunkumar.dindu@knowledgelens.com>
2025-11-19 16:54:15 +0530

    SW js changes

--- public/sw.js
+++ public/sw.js
@@ -2,14 +2,14 @@ import Toaster from "../src/Shared/Utils/Toaster";
 
 self.addEventListener("push", function (event) {
     console.log('Pushing',event)
-    Toaster.success('event','success')
-    Toaster.success(event.title,'success')
-    Toaster.success(event.body,'success')
+    // Toaster.success('event','success')
+    // Toaster.success(event.title,'success')
+    // Toaster.success(event.body,'success')
     
-    const data = event;
+    const data = event || {};
     // const data = {};
 
-    const title = data.title || "New Notification";
+    const title = data?.title || "New Notification";
     const options = {
         body: data.body,
         icon: "/logo192.png",
