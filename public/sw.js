bb7e221b50c165294dac0e70522588808a0b916c
Dindu ArunKumar Reddy <arunkumar.dindu@knowledgelens.com>
2025-11-20 10:38:39 +0530

    SW.js update for dynamic data

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,32 @@
-self.addEventListener("push", function (event) {
-    console.log('Pushing',event)
+// self.addEventListener("push", function (event) {
+//     console.log('Pushing',event)
     
-    const data = event || {};
-    // const data = {};
+//     const data = event || {};
+//     // const data = {};
 
-    const title = "New Notification";
+//     const title = "New Notification";
+//     const options = {
+//         body: 'Data From Backend',
+//         icon: "/logo192.png",
+//         badge: "/logo192.png",
+//     };
+
+//     event.waitUntil(self.registration.showNotification(title, options));
+// });
+self.addEventListener("push", function (event) {
+    console.log("Push event received:", event);
+
+    let data = {};
+    if (event.data) {
+        data = event.data.json(); 
+    }
+
+    const title = data?.title || "New Notification";
     const options = {
-        body: 'Data From Backend',
+        body: data?.body || "No message",
         icon: "/logo192.png",
         badge: "/logo192.png",
+        data: data
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
