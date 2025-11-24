33d5ff91858043cb603e07dea9195eda77ae2eaf
Dindu ArunKumar Reddy <arunkumar.dindu@knowledgelens.com>
2025-11-24 17:59:17 +0530

    pwa except scripts

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,19 @@
-// const { override, addWebpackPlugin } = require("customize-cra");
-// const WorkboxPlugin = require("workbox-webpack-plugin");
+const { override, addWebpackPlugin } = require("customize-cra");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
-// module.exports = override(
-//   addWebpackPlugin(
-//     new WorkboxPlugin.GenerateSW({
-//       clientsClaim: true,
-//       skipWaiting: true,
-//       swDest: "pwa-sw.js",
-//       maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-//       runtimeCaching: [
-//         {
-//           urlPattern: /\.(?:png|jpg|jpeg|svg|gif|js|css|html)$/,
-//           handler: "CacheFirst",
-//         }
-//       ]
-//     })
-//   )
-// );
+module.exports = override(
+  addWebpackPlugin(
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      swDest: "pwa-sw.js",
+      maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|js|css|html)$/,
+          handler: "CacheFirst",
+        }
+      ]
+    })
+  )
+);
